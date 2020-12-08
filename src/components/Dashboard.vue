d1e3128c57a73654b763ee245c7f48c8f3bec139
ellankz <enelipa@gmail.com>
2020-12-08 10:53:22 +0200

    feat: add mock data and create main components

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Dashboard</h1>
+  <Map />
+  <Table v-bind:data="data"/>
+  <Chart />
+  <List />
+</template>
+
+<script>
+import { getData } from '../service/data';
+import Map from './Map.vue';
+import List from './List.vue';
+import Table from './Table.vue';
+import Chart from './Chart.vue';
+
+export default {
+  name: 'Dashboard',
+  components: {
+    Map,
+    Table,
+    Chart,
+    List,
+  },
+  data() {
+    return {
+      data: getData(),
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
