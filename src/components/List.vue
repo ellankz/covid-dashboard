432be13d4babe6db89d13f4c7d15f39769e865e0
Aleksandr Zubakin <eldoson@yandex.ru>
2020-12-16 10:56:53 +0500

    init: basic data

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,18 +1,15 @@
 <template>
-  <div>
-    <h2>List</h2>
-    <div v-if="loadingState.loading" class="loading">
-      Loading...
-    </div>
-
-    <div v-if="loadingState.error" class="error">
-      {{ error }}
-    </div>
-
-    <div v-if="data" class="content">
-      <p>total: {{ data.Global.Summary.Total.Confirmed }}</p>
-    </div>
-  </div>
+  <h2>List</h2>
+   <input type="text" placeholder="Search country">
+   <ul>
+    <li :key="countryCases" v-for="countryCases in countriesCases"
+    @click="getTotal(countryCases)">
+    <span>{{countryCases.country}}</span>
+    <span>cases: {{ getValues(countryCases, 'cases') }}</span>
+    <span>death: {{ getValues(countryCases, 'deaths') }}</span>
+    <span>recovered: {{ getValues(countryCases, 'recovered') }}</span>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -23,7 +20,28 @@ export default {
     data: Object,
     loadingState: Object,
   },
-  methods: {},
+  data() {
+    return {
+      countriesCases: this.data.Countries,
+    };
+  },
+  methods: {
+    getValues(countryCases, value) {
+      return Object.values(countryCases.timeline[value])[1];
+    },
+    getTotal(key) {
+      const [yesterdayCases, todayCases] = Object.values(key.timeline.cases);
+      this.totalCases = todayCases;
+      this.lastDayCases = todayCases - yesterdayCases;
+      const [yesterdayDeaths, todayDeaths] = Object.values(key.timeline.deaths);
+      this.totalDeaths = todayDeaths;
+      this.lastDayDeaths = todayDeaths - yesterdayDeaths;
+      const [yesterdayRecovered, todayRecovered] = Object.values(key.timeline.recovered);
+      this.totalRecovered = todayRecovered;
+      this.lastDayRecovered = todayRecovered - yesterdayRecovered;
+      this.world = key.country;
+    },
+  },
 };
 </script>
 
