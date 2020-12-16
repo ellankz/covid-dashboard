b8b51abb1c164b96f56a2e1d3c7d3099fca25b7e
Aleksandr Zubakin <eldoson@yandex.ru>
2020-12-16 19:31:16 +0500

    feat: basic structure

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,18 +1,22 @@
 <template>
-  <h2>List</h2>
-   <input type="text" placeholder="Search country">
-   <ul>
-    <li :key="countryCases" v-for="countryCases in countriesCases"
-    @click="getTotal(countryCases)">
-    <span>{{countryCases.country}}</span>
-    <span>cases: {{ getValues(countryCases, 'cases') }}</span>
-    <span>death: {{ getValues(countryCases, 'deaths') }}</span>
-    <span>recovered: {{ getValues(countryCases, 'recovered') }}</span>
-    </li>
-  </ul>
+  <div class="container">
+    <input type="text" placeholder="Search country">
+    <ul>
+      <li :key="countryCases" v-for="countryCases in countriesCases"
+        @click="getTotal(countryCases)">
+        <span>
+          <img :src="flags[countryCases.code].flag" :alt="countryCases.code">
+          {{countryCases.country}}
+        </span>
+        <span>{{ getValues(countryCases, 'cases') }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import flagsCountries from '../service/countries.json';
+
 export default {
   name: 'List',
   created() {},
@@ -23,6 +27,7 @@ export default {
   data() {
     return {
       countriesCases: this.data.Countries,
+      flags: flagsCountries,
     };
   },
   methods: {
@@ -47,4 +52,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  .container {
+    background-color: $color-gray;
+    padding: 10px;
+  }
+  ul {
+    overflow: auto;
+    height: 300px;
+    margin-left: 0;
+    padding-left: 0;
+  li {
+    width: 100%;
+  }
+  li:hover {
+    background-color: grey;
+  }
+}
+img {
+  margin-right: 10px;
+  width: 20px;
+}
+
+span {
+  margin-right: 10px;
+}
 </style>
