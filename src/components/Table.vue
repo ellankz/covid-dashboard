62b7feb0a185a1317be3ec95ba747ec6d21606c2
Aleksandr Zubakin <eldoson@yandex.ru>
2020-12-17 21:56:00 +0500

    feat: get data by state

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,44 +1,50 @@
 <template>
   <div class="container">
     <div class="options">
-      <div class="location">{{ world }}</div>
+      <div class="location">
+       {{ currentCountry }}
+      </div>
       <div class="switches">
       <div class="all-time">
-        <button class="btn" @click = "getLastDayData(event)">{{btnLastDayText}}</button>
+        <button class="btn" @click = "pressNewBtn(event)">{{ btnLastDayText }}</button>
       </div>
       <div class="absolute">
-        <button class="btn" @click = "getDataBy100k(event)">{{ btn100kText}}</button>
+        <button class="btn" @click = "pressBy100kBtn(event)">{{ btn100kText }}</button>
       </div>
       </div>
     </div>
     <div class="inner-table">
       <div class="total total-cases">
         <div class="title">Total cases:</div>
-        <span v-if ="!isLastDay && !is100k">{{ total.Confirmed.toLocaleString() }}</span>
-        <span v-else-if ="isLastDay && !is100k">+{{lastDay.Confirmed.toLocaleString()}}</span>
-        <span v-else-if ="is100k && !isLastDay">{{ dataBy100k.Confirmed.toFixed(2)}}</span>
-        <span v-else>{{ lastDayBy100k.Confirmed.toFixed(2)}}</span>
+        <span v-if ="!isLastDay && !is100k">{{ getTotal.Confirmed.toLocaleString() }} </span>
+        <span v-else-if ="isLastDay && !is100k">
+          +{{getLastDayData.Confirmed.toLocaleString()}}
+        </span>
+        <span v-else-if ="is100k && !isLastDay">{{ getTotalBy100k.Confirmed.toFixed(2)}}</span>
+        <span v-else>{{ getLastDayBy100k.Confirmed.toFixed(2)}}</span>
       </div>
       <div class="total total-deaths">
         <div class="title">Total deaths:</div>
-        <span v-if ="!isLastDay && !is100k">{{ total.Deaths.toLocaleString() }}</span>
-        <span v-else-if ="isLastDay && !is100k">+{{lastDay.Deaths.toLocaleString()}}</span>
-        <span v-else-if ="is100k && !isLastDay">{{ dataBy100k.Deaths.toFixed(2)}}</span>
-        <span v-else>{{lastDayBy100k.Deaths.toFixed(2)}}</span>
+        <span v-if ="!isLastDay && !is100k">{{ getTotal.Deaths.toLocaleString() }}</span>
+        <span v-else-if ="isLastDay && !is100k">+{{getLastDayData.Deaths.toLocaleString()}}</span>
+        <span v-else-if ="is100k && !isLastDay">{{ getTotalBy100k.Deaths.toFixed(2)}}</span>
+        <span v-else>{{getLastDayBy100k.Deaths.toFixed(2)}}</span>
       </div>
       <div class="total total-recovered">
        <div class="title">Total recovered:</div>
-       <span v-if ="!isLastDay && !is100k">{{ total.Recovered.toLocaleString() }}</span>
-       <span v-else-if ="isLastDay && !is100k">+{{lastDay.Recovered.toLocaleString()}}</span>
-       <span v-else-if ="is100k && !isLastDay">{{ dataBy100k.Recovered.toFixed(2)}}</span>
-       <span v-else>{{ lastDayBy100k.Recovered.toFixed(2)}}</span>
+       <span v-if ="!isLastDay && !is100k">{{ getTotal.Recovered.toLocaleString() }}</span>
+       <span v-else-if ="isLastDay && !is100k">+{{getLastDayData.Recovered.toLocaleString()}}</span>
+       <span v-else-if ="is100k && !isLastDay">{{ getTotalBy100k.Recovered.toFixed(2)}}</span>
+       <span v-else>{{ getLastDayBy100k.Recovered.toFixed(2)}}</span>
       </div>
     </div>
-  <!-- <p>total: </p> -->
   </div>
+  {{  }}
 </template>
 
 <script>
+import flagsCountries from '../service/countries.json';
+
 export default {
   name: 'Table',
   created() {
@@ -46,53 +52,75 @@ export default {
   },
   props: {
     data: Object,
+    state: Object,
   },
   data() {
     return {
-      world: 'World',
+      country: 'World',
       btnLastDayText: 'All time',
-      btn100kText: 'Absolute',
+      btn100kText: 'Total',
       total: this.data.Global.Summary.Total,
       lastDay: this.data.Global.Summary.New,
-      dataBy100k: this.data.Global.Summary.TotalPer100k,
+      totalBy100k: this.data.Global.Summary.TotalPer100k,
       lastDayBy100k: this.data.Global.Summary.NewPer100k,
       isLastDay: false,
       is100k: false,
+      flags: flagsCountries,
     };
   },
   computed: {
     getCases() {
       return this.countriesCases;
     },
+    currentCountry() {
+      if (this.state.country === null) return this.country;
+      return this.data.Countries[this.state.country].country;
+    },
+    getTotal() {
+      if (this.state.country === null) return this.total;
+      return this.data.Countries[this.state.country].timeline.Summary.Total;
+    },
+    getTotalBy100k() {
+      if (this.state.country === null) return this.total;
+      return this.data.Countries[this.state.country].timeline.Summary.TotalPer100k;
+    },
+    getLastDayData() {
+      if (this.state.country === null) return this.lastDay;
+      return this.data.Countries[this.state.country].timeline.Summary.New;
+    },
+    getLastDayBy100k() {
+      if (this.state.country === null) return this.lastDayBy100k;
+      return this.data.Countries[this.state.country].timeline.Summary.NewPer100k;
+    },
+  },
+  watch: {
+    state: {
+      deep: true,
+    },
   },
   methods: {
     logData() {
       console.log(this.data);
     },
-    getTotal(key) {
-      const [yesterdayCases, todayCases] = Object.values(key.timeline.cases);
-      this.totalCases = todayCases;
-      this.lastDayCases = todayCases - yesterdayCases;
-
-      const [yesterdayDeaths, todayDeaths] = Object.values(key.timeline.deaths);
-      this.totalDeaths = todayDeaths;
-      this.lastDayDeaths = todayDeaths - yesterdayDeaths;
-
-      const [yesterdayRecovered, todayRecovered] = Object.values(key.timeline.recovered);
-      this.totalRecovered = todayRecovered;
-      this.lastDayRecovered = todayRecovered - yesterdayRecovered;
-
-      this.world = key.country;
-    },
-    getLastDayData() {
+    pressNewBtn() {
       this.isLastDay = !this.isLastDay;
-      if (!this.isLastDay) this.btnLastDayText = 'All time';
-      else this.btnLastDayText = 'Last day';
+      if (!this.isLastDay) {
+        this.btnLastDayText = 'All time';
+        this.$emit('updatePeriod', 'All time');
+      } else {
+        this.btnLastDayText = 'New';
+        this.$emit('updatePeriod', 'New');
+      }
     },
-    getDataBy100k() {
+    pressBy100kBtn() {
       this.is100k = !this.is100k;
-      if (!this.is100k) this.btn100kText = 'Absolute';
-      else this.btn100kText = 'Per 100k';
+      if (!this.is100k) {
+        this.btn100kText = 'Total';
+        this.$emit('updateCalcType', 'Total');
+      } else {
+        this.btn100kText = 'Per 100k';
+        this.$emit('updateCalcType', 'Per 100k');
+      }
     },
   },
 };
